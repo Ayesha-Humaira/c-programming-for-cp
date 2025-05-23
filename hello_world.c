9c46f42369b0dd194636d4afdab40cd1e89641a2
Ayesha Humaira <144620809+Ayesha-Humaira@users.noreply.github.com>
2025-05-23 14:38:45 +0600

    Initial commit with C codes

---
+++ hello_world.c
@@ -0,0 +1,10 @@
+//https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_3_A
+#include<stdio.h>
+int main()
+{
+    for(int i=1; i<=1000; i++) {
+        printf("Hello World\n");
+    }
+
+    return 0;
+}
